46447be51e42054fd18e241f590c8229749befd7
Julia Isakova <gunoffun@yandex.ru>
2022-03-26 20:32:25 +0300

    update with index and some empty line

--- games/cli-even.js
+++ games/cli-even.js
@@ -1,29 +1,6 @@
 /* eslint-disable semi */
 import readlineSync from 'readline-sync';
-
-const sleep = (milliseconds) => {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-const startGame = () => {
-  console.log();
-  console.log('Welcome to the Brain Games!');
-  console.log();
-  sleep(1000)
-  const name = readlineSync.question('May I have your name? ');
-  sleep(500)
-  console.log();
-  console.log(`Hello, ${name}!`);
-  console.log();
-  sleep(500)
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log();
-  return name;
-}
+import { sleep, startGame } from '../src/index.js';
 
 const randomNumber = () => {
   const number = Math.floor(Math.random() * 100);
@@ -33,20 +10,22 @@ const randomNumber = () => {
 
 const game = () => {
   const name = startGame();
-  sleep(3000)
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log();
+  sleep(3000);
   let counter = 0;
   const no = 'no';
   const yes = 'yes';
   while (counter < 3) {
     const number = randomNumber();
-    sleep(500)
+    sleep(500);
     const ask = readlineSync.question('Your answer: ');
-    sleep(1000)
+    sleep(1000);
     if ((number % 2 === 0 && ask === yes) || (number % 2 > 0 && ask === no)) {
       console.log('Correct!');
       console.log();
       counter += 1;
-      sleep(500)
+      sleep(500);
     } else {
       if (number % 2 === 0) {
         console.log();
@@ -58,7 +37,7 @@ const game = () => {
           Let's try again, ${name}!`;
     }
   }
-  sleep(500)
+  sleep(500);
   return `Congratulations, ${name}!`
 }
 export default game;
