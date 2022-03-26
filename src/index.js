9f7d24464d7182c4b8c3b9500b1679add4eef47f
Julia Isakova <gunoffun@yandex.ru>
2022-03-26 20:33:23 +0300

    add index.js for all games

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const sleep = (milliseconds) => {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+};
+
+const startGame = () => {
+  console.log();
+  console.log('Welcome to the Brain Games!');
+  console.log();
+  sleep(1000);
+  const name = readlineSync.question('May I have your name? ');
+  sleep(500);
+  console.log();
+  console.log(`Hello, ${name}!`);
+  console.log();
+  sleep(500);
+  return name;
+};
+
+export { sleep, startGame };
