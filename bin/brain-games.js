9fa9132c13389cc40d121bf8d561b32df4c37537
Julia Isakova <gunoffun@yandex.ru>
2022-03-28 11:52:42 +0300

    update with delete sleep

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import askAboutName from '../src/cli.js';
 
-console.log(askAboutName());
+askAboutName();
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import game from '../games/cli-prime.js';
 
-console.log(game());
+game();
