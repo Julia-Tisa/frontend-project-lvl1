6245d477c7e0d5c050339c6b874a9163a3256a59
Julia Isakova <gunoffun@yandex.ru>
2022-04-09 02:23:32 +0300

    update brain-even

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,18 +1,20 @@
 import logicOfGame from '../index.js';
 import randomNumber from '../random-number.js';
 
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
 const game = () => {
   const instructions = 'Answer "yes" if the number is even, otherwise answer "no".';
 
   const calculateResult = () => {
-    const no = 'no';
-    const yes = 'yes';
     const number = randomNumber(0, randomNumber(100, 4576));
     const question = `${number}`;
-    let result = no;
-    if (number % 2 === 0) {
-      result = yes;
-    }
+    const result = isEven(number) ? 'yes' : 'no';
     return [question, result];
   };
   logicOfGame(instructions, calculateResult);
