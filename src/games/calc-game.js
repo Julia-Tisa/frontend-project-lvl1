7c7f99d57cc6bc780aadd94c975d04a225582658
Julia Isakova <gunoffun@yandex.ru>
2022-04-09 01:23:42 +0300

    update brain-calc

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,16 +1,9 @@
 import logicOfGame from '../index.js';
 import randomNumber from '../random-number.js';
 
-const randomSign = () => {
-  const num = Math.floor(Math.random() * 3);
-  switch (num) {
-    case 0:
-      return '+';
-    case 1:
-      return '*';
-    default:
-      return '-';
-  }
+const randomSign = (num) => {
+  const sign = ['+', '-', '*'];
+  return sign[num];
 };
 
 const resultExpression = (num1, num2, sign) => {
@@ -19,8 +12,10 @@ const resultExpression = (num1, num2, sign) => {
       return num1 + num2;
     case '*':
       return num1 * num2;
-    default:
+    case '-':
       return num1 - num2;
+    default:
+      throw new Error(`Unknown ${sign}!`);
   }
 };
 
@@ -30,7 +25,7 @@ const game = () => {
   const calculateResult = () => {
     const num1 = randomNumber(0, 100);
     const num2 = randomNumber(0, 100);
-    const sign = randomSign();
+    const sign = randomSign(randomNumber(0, 2));
     const result = String(resultExpression(num1, num2, sign));
     const question = `${num1} ${sign} ${num2}`;
     return [question, result];
