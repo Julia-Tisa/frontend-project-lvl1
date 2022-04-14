6dc61b663d1701ff0408ee483f7b5ca38a79aa72
Julia Isakova <gunoffun@yandex.ru>
2022-04-15 01:32:30 +0300

    update function isPrime with number < 2

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -2,6 +2,9 @@ import engineOfGame from '../index.js';
 import randomNumber from '../random-number.js';
 
 const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
