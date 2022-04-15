1c9a45312745b8e4a5df7f5e94bfd17efd70a18a
Julia Isakova <gunoffun@yandex.ru>
2022-04-15 05:22:44 +0300

    update progression-game

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,25 +1,13 @@
 import engineOfGame from '../index.js';
 import generateRandomNumber from '../generate-random-number.js';
 
-const randomArrayofNumbers = (length, step) => {
+const generateProgression = (length, step, start) => {
   const randomArray = [];
-  const start = generateRandomNumber(1, 50);
   for (let i = 0; i < length; i += 1) {
     const val = start + step * i;
     randomArray.push(val);
   }
   return randomArray;
-};
-
-const missingInProgression = (randomArray, lengthArray) => {
-  const lenght = lengthArray - 1;
-  const missing = generateRandomNumber(0, lenght);
-  const randomArrayWithMissing = [];
-  for (let i = 0; i < randomArray.length; i += 1) {
-    randomArrayWithMissing.push(randomArray[i]);
-  }
-  randomArrayWithMissing[missing] = '..';
-  return randomArrayWithMissing;
 };
 
 const instructions = 'What number is missing in the progression?';
@@ -28,11 +16,12 @@ const game = () => {
   const generateRoundData = () => {
     const length = generateRandomNumber(5, 10);
     const step = generateRandomNumber(2, 18);
-    const randomArray = randomArrayofNumbers(length, step);
-    const randomArrayWithMissing = missingInProgression(randomArray, randomArray.length);
-    const index = randomArrayWithMissing.indexOf('..');
-    const correctAnswer = String(randomArray[index]);
-    const question = `${randomArrayWithMissing.join(' ')}`;
+    const start = generateRandomNumber(1, 50);
+    const randomProgression = generateProgression(length, step, start);
+    const missing = generateRandomNumber(0, randomProgression.length - 1);
+    const correctAnswer = String(randomProgression[missing]);
+    randomProgression[missing] = '..';
+    const question = `${randomProgression.join(' ')}`;
     return [question, correctAnswer];
   };
   engineOfGame(instructions, generateRoundData);
